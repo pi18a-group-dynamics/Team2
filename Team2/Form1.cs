c64aa1206b0d64665622817740ac9a4166453f0b
ilya0047km <48825256+ilya0047km@users.noreply.github.com>
2020-05-30 19:36:46 +0300

    Реализован поиск

--- Team2/Form1.cs
+++ Team2/Form1.cs
@@ -10,6 +10,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using Npgsql;
 
+//create table pdata(person_id serial NOT NULL,first_name character varying(20) NOT NULL,patronymic character varying(20) NOT NULL,series character varying(2) NOT NULL,who_gave character varying(30) NOT NULL,when_gave character varying(30),date_of_birth date NOT NULL,children character varying(100),photo character varying(100),residence character varying(30),place_of_birth character varying(30) NOT NULL,"number" character varying(6) NOT NULL,nationality character varying(20) NOT NULL,last_name character varying(20) NOT NULL,place_code character varying(10),PRIMARY KEY (person_id)); 
 
 
 namespace Team2
@@ -18,7 +19,7 @@ namespace Team2
         
     {
         public static String filePath = "";
-        public static String connectionString = "Server=localhost;Port=5432;User Id=postgres;Password=postgres;Database=Passport;";
+        public static String connectionString = "Server=localhost;Port=5432;User Id=postgres;Password=postgres;Database=passport;";
         public NpgsqlConnection connection = new NpgsqlConnection(connectionString);
         
         public Form1()
@@ -67,10 +68,10 @@ namespace Team2
             
 
 
-/* 
+
            // рабочий SELECT (Илья, для поиска пригодится)
 
-            NpgsqlCommand command = new NpgsqlCommand("SELECT * FROM pdata", connection);
+           /* NpgsqlCommand command = new NpgsqlCommand("SELECT * FROM pdata", connection);
             NpgsqlDataReader dataReader = command.ExecuteReader();
             if (dataReader.HasRows)
             {
@@ -107,9 +108,191 @@ namespace Team2
 
         private void button3_Click(object sender, EventArgs e)
         {
-            FormSearch search = new FormSearch();
-            search.Show();
-            /* 
+            connection.Open();
+
+            String last_name = textBox1.Text;
+            String first_name = textBox2.Text;
+            String patronymic = textBox3.Text;
+            String nationality = textBox4.Text;
+            String series = textBox5.Text;
+            String number = textBox6.Text;
+            String who_gave = textBox7.Text;
+            String when_gave = textBox12.Text;
+            String place_code = textBox8.Text;
+            String place_of_birth = textBox13.Text;
+            String date_of_birth = textBox9.Text;
+            String residence = textBox10.Text;
+            String children = textBox11.Text;
+
+
+            if(number != "" && series != ""){
+                NpgsqlCommand command = new NpgsqlCommand("SELECT * FROM pdata WHERE number = '"+number+"' and series = '"+series+"';", connection);
+                NpgsqlDataReader dataReader = command.ExecuteReader();
+
+                if (dataReader.HasRows)
+                {
+                    foreach (DbDataRecord dbDataRecord in dataReader)
+                    {
+                        textBox1.Text = dbDataRecord["last_name"].ToString();
+                        textBox2.Text = dbDataRecord["first_name"].ToString();
+                        textBox3.Text = dbDataRecord["patronymic"].ToString();
+                        textBox4.Text = dbDataRecord["nationality"].ToString();
+                        textBox7.Text = dbDataRecord["who_gave"].ToString();
+                        textBox12.Text = dbDataRecord["when_gave"].ToString();
+                        textBox8.Text = dbDataRecord["place_code"].ToString();
+                        textBox13.Text = dbDataRecord["place_of_birth"].ToString();
+                        textBox9.Text = dbDataRecord["date_of_birth"].ToString();
+                        textBox10.Text = dbDataRecord["residence"].ToString();
+                        textBox11.Text = dbDataRecord["children"].ToString();
+                        pictureBox1.Image = Image.FromFile(dbDataRecord["photo"].ToString());
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Гражданина не существует");
+                }
+            }
+            else
+            {
+                String whereCommand = "";
+
+                if (textBox2.Text != "")
+                {
+                    if(whereCommand != "")
+                    {
+                        whereCommand = whereCommand + " and ";
+                    }
+                    whereCommand = whereCommand + "first_name = '" + first_name + "'";
+                }
+                if (textBox1.Text != "")
+                {
+                    if (whereCommand != "")
+                    {
+                        whereCommand = whereCommand + " and ";
+                    }
+                    whereCommand = whereCommand + "last_name = '" + last_name + "'";
+                }
+                if (textBox3.Text != "")
+                {
+                    if (whereCommand != "")
+                    {
+                        whereCommand = whereCommand + " and ";
+                    }
+                    whereCommand = whereCommand + "patronymic = '" + patronymic + "'";
+                }
+                if (textBox4.Text != "")
+                {
+                    if (whereCommand != "")
+                    {
+                        whereCommand = whereCommand + " and ";
+                    }
+                    whereCommand = whereCommand + "nationality = '" + nationality + "'";
+                }
+                if (textBox5.Text != "")
+                {
+                    if (whereCommand != "")
+                    {
+                        whereCommand = whereCommand + " and ";
+                    }
+                    whereCommand = whereCommand + "series = '" + series + "'";
+                }
+                if (textBox6.Text != "")
+                {
+                    if (whereCommand != "")
+                    {
+                        whereCommand = whereCommand + " and ";
+                    }
+                    whereCommand = whereCommand + "number = '" + number + "'";
+                }
+                if (textBox7.Text != "")
+                {
+                    if (whereCommand != "")
+                    {
+                        whereCommand = whereCommand + " and ";
+                    }
+                    whereCommand = whereCommand + "who_gave = '" + who_gave + "'";
+                }
+                if (textBox12.Text != "")
+                {
+                    if (whereCommand != "")
+                    {
+                        whereCommand = whereCommand + " and ";
+                    }
+                    whereCommand = whereCommand + "when_gave = '" + when_gave + "'";
+                }
+                if (textBox8.Text != "")
+                {
+                    if (whereCommand != "")
+                    {
+                        whereCommand = whereCommand + " and ";
+                    }
+                    whereCommand = whereCommand + "place_code = '" + place_code + "'";
+                }
+                if (textBox13.Text != "")
+                {
+                    if (whereCommand != "")
+                    {
+                        whereCommand = whereCommand + " and ";
+                    }
+                    whereCommand = whereCommand + "place_of_birth = '" + place_of_birth + "'";
+                }
+                if (textBox9.Text != "")
+                {
+                    if (whereCommand != "")
+                    {
+                        whereCommand = whereCommand + " and ";
+                    }
+                    whereCommand = whereCommand + "date_of_birth = '" + date_of_birth + "'";
+                }
+                if (textBox10.Text != "")
+                {
+                    if (whereCommand != "")
+                    {
+                        whereCommand = whereCommand + " and ";
+                    }
+                    whereCommand = whereCommand + "residence = '" + residence + "'";
+                }
+                if (textBox11.Text != "")
+                {
+                    if (whereCommand != "")
+                    {
+                        whereCommand = whereCommand + " and ";
+                    }
+                    whereCommand = whereCommand + "children = '" + children + "'";
+                }
+                if (filePath != "")
+                {
+                    if (whereCommand != "")
+                    {
+                        whereCommand = whereCommand + " and ";
+                    }
+                    whereCommand = whereCommand + "photo = '" + filePath + "'";
+                }
+                if (whereCommand != "")
+                {
+                    NpgsqlCommand command = new NpgsqlCommand("SELECT * FROM pdata WHERE " + whereCommand +";" , connection);
+                    NpgsqlDataReader dataReader = command.ExecuteReader();
+                    if (dataReader.HasRows)
+                    {
+                        Console.WriteLine("Таблица: example");
+                        Console.WriteLine("id value");
+                        foreach (DbDataRecord dbDataRecord in dataReader)
+                            MessageBox.Show(dbDataRecord["person_id"] + "   " + dbDataRecord["last_name"] + "   " + dbDataRecord["first_name"] + "   " + dbDataRecord["patronymic"] + "   " + dbDataRecord["nationality"] + "   "
+                                + dbDataRecord["series"] + "   " + dbDataRecord["number"] + "   " + dbDataRecord["who_gave"] + "   " + dbDataRecord["when_gave"] + "   " +
+                                dbDataRecord["place_code"] + "   " + dbDataRecord["place_of_birth"] + "   " + dbDataRecord["date_of_birth"] + "   " + dbDataRecord["residence"] +
+                                "   " + dbDataRecord["children"] + "   " + dbDataRecord["photo"]);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Соотвествия не найдены");
+                    }
+                }
+
+            }
+
+            connection.Close();
+
+            /*
             // вообще тут будет поиск, но              
             
             //так будет работать удаление:
